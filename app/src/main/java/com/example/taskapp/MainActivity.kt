69228e61747349eea85d762431c4cb7343d7c052
pasindu <IT22355928@my.sliit.lk>
2024-05-11 16:46:01 +0530

    done project

---
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.taskapp
+
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskmanagerapp.Adapter.ToDoAdapter
+import com.example.taskapp.Model.ToDoModel
+import com.example.taskapp.Utils.DatabaseHandler
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity(), DialogCloseListener {
+
+    private lateinit var db: DatabaseHandler
+    private lateinit var tasksRecyclerView: RecyclerView
+    private lateinit var tasksAdapter: ToDoAdapter
+    private lateinit var fab: FloatingActionButton
+    private lateinit var taskList: List<ToDoModel>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        db = DatabaseHandler(this)
+        db.openDatabase()
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView)
+        tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        tasksAdapter = ToDoAdapter(db, this@MainActivity)
+        tasksRecyclerView.adapter = tasksAdapter
+
+        val itemTouchHelper = ItemTouchHelper(RecyclerItemTouchHelper(tasksAdapter))
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView)
+
+        fab = findViewById(R.id.fab)
+
+        taskList = db.getAllTasks().reversed()
+        tasksAdapter.setTasks(taskList)
+
+        fab.setOnClickListener {
+            AddNewTask.newInstance().show(supportFragmentManager, AddNewTask.TAG)
+        }
+    }
+
+    override fun handleDialogClose(dialog: DialogInterface?) {
+        taskList = db.getAllTasks().reversed()
+        tasksAdapter.setTasks(taskList)
+        tasksAdapter.notifyDataSetChanged()
+    }
+}
